29ada430f704e862feadaf099bb3b28651f68f29
hans <focus5n@naver.com>
2022-12-24 20:17:43 +0900

    STD REACT-REDUX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {createStore} from "redux";
+import {Provider, useSelector, useDispatch} from "react-redux";
+
+function reducer(currentState, action) {
+    const newState = {...currentState}
+
+    if (currentState === undefined) {
+        return {number: 1, type: action.type}
+    }
+    if (action.type === "PLUS") {
+        newState.number++;
+    }
+
+    return newState;
+}
+const store = createStore(reducer);
+
+function Left01() {
+  return <div>
+      <h1>Left01</h1>
+      <Left02></Left02>
+  </div>
+}
+
+function Left02() {
+    return <div>
+        <h1>Left02</h1>
+        <Left03></Left03>
+    </div>
+}
+
+function Left03() {
+    const number = useSelector(state => state.number);
+
+    return <div>
+        <h1>Left03 : {number}</h1>
+    </div>
+}
+
+function Right01() {
+    return <div>
+        <h1>Right01</h1>
+        <Right02></Right02>
+    </div>
+}
+
+function Right02() {
+    return <div>
+        <h1>Right02</h1>
+        <Right03></Right03>
+    </div>
+}
+
+function Right03() {
+    const dispatch = useDispatch();
+
+    return <div>
+        <h1>Right03</h1>
+        <input type="button" value="+" onClick={() => {
+            dispatch({type: "PLUS"})
+        }}/>
+    </div>
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Grid">
+            <Provider store={store}>
+              <Left01></Left01>
+              <Right01></Right01>
+            </Provider>
+        </div>
     </div>
   );
 }
